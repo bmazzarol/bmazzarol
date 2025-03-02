2199caa2ec6db687725e925d609be26f3be7f95e
Ben Mazzarol <bmazzarol@gmail.com>
2025-03-02 12:38:09 +0800

    feat: Add fluidic to mono-repo

---
+++ scripts/add-dotnet-project.ps1
@@ -0,0 +1,6 @@
+﻿$projectName = Read-Host "Enter the name of the project"
+$projectPath = "projects/dotnet/$projectName"
+
+$PSScriptRoot = Split-Path -Parent $PSScriptRoot
+
+git submodule add "https://github.com/bmazzarol/$projectName.git" $projectPath
