dad012af73dc6b958ab26103c9b579076d12fae2
Ben Mazzarol <bmazzarol@gmail.com>
2026-02-22 13:41:34 +0800

    Add dependencies and generated files for Source Generator utilities
    Included project.assets.json and added necessary analyzers and dependencies for Source Generator utilities.

---
+++ projects/dotnet/Internal/README.md
@@ -0,0 +1,4 @@
+# Internal Projects
+
+These are projects that are not published. They cannot be directly referenced in
+other projects. Their contents must be copied into the consuming project.
---
+++ projects/dotnet/Internal/SourceGeneratorUtilities/Result.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SourceGeneratorUtilities;
+
+internal abstract record Result<T>
+{
+    internal sealed record Success(T Value) : Result<T>;
+
+    internal sealed record Failure(DiagnosticDescriptor Descriptor) : Result<T>;
+
+    public static implicit operator Result<T>(T success) => new Success(success);
+
+    public static implicit operator Result<T>(DiagnosticDescriptor failure) => new Failure(failure);
+}
